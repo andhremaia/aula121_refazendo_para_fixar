762a0edd6cacb0444e4be01065c92cb3b6878377
andhremaia <andhremaia@gmail.com>
2021-07-30 19:01:17 -0300

    Implementando o classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,33 @@
 package application;
 
-import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
 
 public class Program {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		
-		Product p = new Product("TV", 1000.00);
-		System.out.println(p);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Client client = new Client();
+		
+		
+		System.out.println("Enter cliente data: \n");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("E-mail: ");
+		//sc.nextLine();
+		String email = sc.nextLine();
+		
+		
+		
+		
+		
+		sc.close();
+	
 		
 	}
 }
